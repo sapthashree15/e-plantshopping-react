ed8ef05ae343e3d24328a98089139ced1c8d5ef7
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-09 09:51:22 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,7 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    const {name, image, cost} = action.payload;
-    const existingItem = state.items.find(item => item.name === name);
-    if (existingItem) {
-      existingItem.quantity++;
-    } else {
-      state.items.push({name, image ,cost, quantity: 1});
+    
     }
     },
 
