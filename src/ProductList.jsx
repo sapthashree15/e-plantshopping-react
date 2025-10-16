f239fb19da31bb42c7d7ab622a7126d10e714d43
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-16 20:09:51 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -223,7 +223,7 @@ function ProductList() {
     padding: '15px',
     display: 'flex',
     justifyContent: 'space-between',
-    alignIems: 'center',
+    alignItems: 'center',
     fontSize: '20px',
    }
    const styleObjUl={
